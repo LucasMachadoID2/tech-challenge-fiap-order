24be72b73a4249f6acaf6acc55990b86f9dec05f
LucasMachado <contato.lucasmmachado@gmail.com>
2025-04-17 10:36:38 -0300

    Start do projeto

---
+++ src/main/java/com/tech_challenge_fiap/TechChallengeFiapApplication.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_challenge_fiap/TechChallengeFiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_fiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
