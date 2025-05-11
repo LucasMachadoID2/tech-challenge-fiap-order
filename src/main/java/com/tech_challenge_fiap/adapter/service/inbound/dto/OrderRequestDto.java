1ede27c503166bb39bed71aba62096323f1d07da
Guilherme Garcia <guilherme.garcia@ifood.com.br>
2025-05-11 17:11:21 -0300

    test build

--- src/main/java/com/tech_challenge_fiap/adapter/service/inbound/dto/OrderRequestDto.java
+++ src/main/java/com/tech_challenge_fiap/adapter/service/inbound/dto/OrderRequestDto.java
@@ -1,4 +1,4 @@
-package com.tech_challenge_fiap.adapter.service.inbound.dto;
+package com.tech_challenge_fiap.adapter.service.inbound.dtosssss;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
