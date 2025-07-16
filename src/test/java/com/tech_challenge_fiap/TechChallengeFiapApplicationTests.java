c73ce3a7f7af2c37e83087e85d216029ece11228
Guilherme Garcia <guilherme.garcia@ifood.com.br>
2025-07-15 22:46:45 -0300

    Add Mockito to mock the PaymentStatusMigration

--- src/test/java/com/tech_challenge_fiap/TechChallengeFiapApplicationTests.java
+++ src/test/java/com/tech_challenge_fiap/TechChallengeFiapApplicationTests.java
@@ -1,10 +1,23 @@
 package com.tech_challenge_fiap;
 
+import com.tech_challenge_fiap.scripts.PaymentStatusMigration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class TechChallengeFiapApplicationTests {
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public PaymentStatusMigration paymentStatusMigration() {
+			return mock(PaymentStatusMigration.class);
+		}
+	}
 
 	@Test
 	void contextLoads() {
