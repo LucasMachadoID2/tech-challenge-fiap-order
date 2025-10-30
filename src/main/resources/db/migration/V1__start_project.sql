f1cbfb617452842485424f239fe5f8b14b8a28d0
Lucas Machado <contato.lucasmmachado@gmail.com>
2025-10-30 19:53:00 -0300

    refactor/card-56: Corrige banco

--- src/main/resources/db/migration/V1__start_project.sql
+++ src/main/resources/db/migration/V1__start_project.sql
@@ -1,14 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto; -- para gen_random_uuid()
+
 CREATE TABLE clients
 (
-    id PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255),
-    cpf VARCHAR(11),
-    email VARCHAR(100)
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) UNIQUE,
+    email VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE payments
 (
-    id PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     qrCode TEXT,
     qrImage TEXT,
     status VARCHAR(50)
@@ -16,7 +18,7 @@ CREATE TABLE payments
 
 CREATE TABLE products
 (
-    id PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     category VARCHAR(50),
     name VARCHAR(255),
     description VARCHAR(500),
@@ -28,21 +30,20 @@ CREATE TABLE products
 
 CREATE TABLE orders
 (
-    id PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     status VARCHAR(50),
-    client_id INTEGER,
-    payment_id INTEGER,
+    client_id UUID,
+    payment_id UUID,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_order_client FOREIGN KEY (client_id) REFERENCES clients(id),
     CONSTRAINT fk_order_payment FOREIGN KEY (payment_id) REFERENCES payments(id)
 );
 
-
 CREATE TABLE products_orders
 (
-    product_id INTEGER,
-    order_id INTEGER,
+    product_id UUID,
+    order_id UUID,
     PRIMARY KEY (order_id, product_id),
     CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id),
     CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id)
